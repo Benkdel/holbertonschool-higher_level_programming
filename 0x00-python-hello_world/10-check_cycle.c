c56a45bf2b840e65e43c476046fc2c99fe252ea4
Carlos Bentin <cbentin12@gmail.com>
2021-12-20 19:38:59 -0500

    Adding task 10 - find if linked list has an infinite loop 1) using Floyd's Cycle-Finding algorithm 2) adding test sub dir and files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list has loop.
+ * using Floyd's Cycle-Finding algorithm
+ * @list: linked list
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+
+int check_cycle(listint_t *list)
+{
+        listint_t *slow = list, *fast = list;
+
+        if (list == NULL)
+                return (0);
+
+        while (fast && fast->next && fast->next->next)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast)
+                        return (1);
+        }
+        return (0);
+}
