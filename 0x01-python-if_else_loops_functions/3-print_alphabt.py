b9748a7821c3dfd5525941ff9da769aeaf2a509e
Carlos Bentin <cbentin12@gmail.com>
2021-12-21 16:24:42 -0500

    fixing doc style

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for l in range(97, 123):
-        if l != 101 and l != 113: 
-                print("{:c}".format(l), end="")
+    if l != 101 and l != 113:
+        print("{:c}".format(l), end="")
