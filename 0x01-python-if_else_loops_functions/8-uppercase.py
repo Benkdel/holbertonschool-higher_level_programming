49e60001752bfc3e81e5b0a90d4e1f573ac89297
Carlos Bentin <cbentin12@gmail.com>
2021-12-21 16:49:14 -0500

    Fix styling errors

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    conv = 0
     for c in str:
         """ ASCII code convert ord(c) """
         if (ord(c) >= 97 and ord(c) <= 122):
             conv = 32
         else:
             conv = 0
-        print(chr(ord(c) - conv), end="")
+        print("{:c}".format(chr(ord(c) - conv)), end="")
     print()
