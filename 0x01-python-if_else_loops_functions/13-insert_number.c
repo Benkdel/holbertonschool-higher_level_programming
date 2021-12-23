409018cccc4ba4f083c0bb11a7c1d98fb12beabb
Carlos Bentin <cbentin12@gmail.com>
2021-12-23 13:44:32 -0500

    fix betty style

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -4,6 +4,7 @@
  * insert_node - inserts node at given posittion
  * @head: pointer to head of linked list
  * @number: number to add
+ * Return: new node or null if failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
