167724b074e0dba8f1a02da99cb1939b1ee09000
Carlos Bentin <cbentin12@gmail.com>
2021-12-23 15:46:07 -0500

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,20 @@
-if __name__ == "__main__":
-    import sys
-    len_argv = len(sys.argv)
-    argument = "arguments:"
-    if len_argv == 1:
-        argument = "arguments."
-    if len_argv == 2:
-        argument = "argument:"
-    print("{0} {1}".format(len_argv - 1, argument))
-    for i in range(1, len_argv):
-        print("{0}: {1}".format(i, sys.argv[i]))
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    argv = sys.argv
+    lenArg = len(argv) - 1
+    pos = 0
+    if lenArg == 0:
+        print("{:d} arguments.".format(lenArg))
+    elif lenArg == 1:
+        print("{:d} argument:".format(lenArg))
+        for i in argv:
+            if pos != 0:
+                print("{:d}: {}".format(pos, i))
+            pos += 1
+    else:
+        print("{:d} arguments:".format(lenArg))
+        for i in argv:
+            if pos != 0:
+                print("{:d}: {}".format(pos, i))
+            pos += 1
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4 as hid
-    for item in dir(hid):
+import hidden_4
+
+if __name__ == '__main__':
+    for item in dir(hidden_4):
         if item[0:2] != "__":
             print(item)
