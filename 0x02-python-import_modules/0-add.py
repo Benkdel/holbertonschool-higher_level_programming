b86abd13fae4cd76ae6d78ab763015d1f79f005d
Carlos Bentin <cbentin12@gmail.com>
2021-12-23 15:20:45 -0500

    fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,16 +2,10 @@
 import sys
 from add_0 import add
 
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+a = 1
+b = 2
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
-        import sys
-        add(int(sys.argv[1]), int(sys.argv[2]))
-
-
-
-
-
+    import sys
+    add(int(sys.argv[1]), int(sys.argv[2]))
