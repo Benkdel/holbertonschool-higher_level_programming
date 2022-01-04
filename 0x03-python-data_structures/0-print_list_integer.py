0ac9007409e1d7d41290819112d6378496276356
Carlos Bentin <cbentin12@gmail.com>
2022-01-03 20:56:46 -0500

    removing exe validation

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == 'main':
-    def print_list_integer(my_list=[]):
-        for item in my_list:
-            print("{:d}".format(item))
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        print("{:d}".format(item))
