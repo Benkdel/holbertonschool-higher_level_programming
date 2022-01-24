9b0d14bb3ff20aeeba02bbbba92aba9bbe65017e
Carlos Bentin <cbentin12@gmail.com>
2022-01-24 15:13:20 -0500

    First commit 1) Mandatory task completed from 0 to 8 2) Documentation is not written 3) Files for task 9 and advanced task 1 added to work them later

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    Task 1 - Mandatory
+"""
+
+
+class Rectangle:
+    """
+        Real definition of a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
