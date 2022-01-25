e7822dccbe0e68b4e74cc1613d2826dd8b942a45
Carlos Bentin <cbentin12@gmail.com>
2022-01-25 07:51:35 -0500

    changing constructor var initialization - self.__var to self.var

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle:
             width (int): Width of the Rectangle
             height (int): Height of the Rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
