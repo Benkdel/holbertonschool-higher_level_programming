ec523ad565a1c1deee3bc071d1118f56afcf3762
Carlos Bentin <cbentin12@gmail.com>
2022-01-28 19:12:25 -0500

    fix

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-
-"""
-    Task 4 - Mandatory
-"""
+"""Creting the Function print_square"""
 
 
 def print_square(size):
-    """
-        Method - prints in stdout the square with the character #.
-    """
-
-    if type(size) is not int:
+    """Function that prints a square with the character #"""
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
     for i in range(size):
-        for j in range(size):
-            print("#", end='')
-        else:
-            print()
+        print("#" * size)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""
-    Task 5 - Mandatory
-"""
+"""Text indentation"""
 
 
 def text_indentation(text):
     """
-        Prints a text with 2 new lines after each of the characters:
-        ".", "?", ":"
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    text is the text to evaluate
     """
-    if type(text) is not str:
-        TypeError("text must be a string")
-    line = ""
-    for _char in text:
-        line = line + _char
-        if (_char) in [".", "?", ":"]:
-            print(line.strip(), end="\n\n")
-            line = ""
-    print(line.strip())
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    lines, start, end = ([], 0, 0)
+    for c in text:
+        if c in ['.', '?', ':']:
+            lines.append(text[start:end + 1])
+            start = end + 1
+        end += 1
+
+    if not text[-1] in ['.', '?', ':']:
+        lines.append(text[start:])
+
+    lines = list(map(lambda line: line.strip(), lines))
+    for line in lines:
+        print(line, end="")
+        if line != lines[-1]:
+            print()
+            print()
