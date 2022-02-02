8a2dcb280f7b7973288e127f0234a75437b5e4be
Carlos Bentin <cbentin12@gmail.com>
2022-02-02 07:18:38 -0500

    fix: changing w to r

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,5 +10,5 @@ def load_from_json_file(filename):
     """
         Creates an Object from JSON file
     """
-    with open(filename, mode="w", encoding="utf-8") as dest_file:
+    with open(filename, mode="r", encoding="utf-8") as dest_file:
         return (json.load(dest_file))
