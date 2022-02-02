31bf805752ed195e140dd4131319953446c0d0a4
Carlos Bentin <cbentin12@gmail.com>
2022-02-02 07:20:28 -0500

    fix style errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,17 +4,19 @@
 """
 
 import sys
-import os.path
+import os
+
 
 save_file = __import__("5-save_to_json_file").save_to_json_file
 load_file = __import__("6-load_from_json_file").load_from_json_file
 
+filename = "add_item.json"
 my_list = []
 
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+if os.path.exists(filename):
+    my_list = load_file(filename)
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
-save_file(my_list, "add_item.json")
+save_file(my_list, filename)
