fd49d611f488b22ab4a4db72b878629b570507fc
Carlos Bentin <cbentin12@gmail.com>
2022-02-02 07:27:28 -0500

    fixing style errors and adding adv tasks:

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
     Functions:
         a) lookup
 """
 
 
 def lookup(obj):
-    """ 
+    """
         Functions - List of available attributes and methods of an object:
         Args:
             obj (object): Any instance
---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Class:
+        a) MyInt.
+"""
+
+
+class MyInt(int):
+    """
+        Class - MyInt class (object) inherits from Int.
+    """
+
+    def __eq__(self, other):
+        """
+            Method (custom) - Invert the function == for !=
+        """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """
+            Method (custom) - Invert the function != for ==
+        """
+        return int.__eq__(self, other)
