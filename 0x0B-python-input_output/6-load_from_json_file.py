b83587a50c34c26c53d1293712ef92a829068228
Carlos Bentin <cbentin12@gmail.com>
2022-02-02 07:15:17 -0500

    fix: changing methods load and dump to loadS and dumpS

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,6 +11,6 @@ def load_from_json_file(filename):
         Creattes an Object from JSON file
     """
     with open(filename, mode="w", encoding="utf-8") as dest_file:
-        json.load(dest_file)
+        json.loads(dest_file)
     
     dest_file.close()
