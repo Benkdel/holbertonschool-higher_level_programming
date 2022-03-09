8f4a0ddcabadce4871f7559c392b00dac5d88738
Carlos Bentin <cbentin12@gmail.com>
2022-03-09 18:39:38 -0500

    fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,10 @@
 -- create table cities referencing state_id to table states id column
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    state_id INT NOT NULL FOREIGN KEY (states) REFERENCES states(id),
-    name VARCHAR(256) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 )
