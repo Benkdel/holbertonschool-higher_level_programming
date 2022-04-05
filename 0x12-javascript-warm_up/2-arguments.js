e42e79e7dc94b64c6f9dec13fb1c350ba7975791
Carlos Bentin <cbentin12@gmail.com>
2022-04-05 09:10:00 -0500

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
-import { argv } from 'process';
+const process = require('process')
 
-
+if (process.argv.length <= 2)
+    console.log("No argument");
+else if (process.argv.length == 3)
+    console.log("Argument found");
+else
+    console.log("Arguments found");
