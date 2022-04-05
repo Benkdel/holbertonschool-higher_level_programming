f61175928741a855944096058fd41be71a0252ab
Carlos Bentin <cbentin12@gmail.com>
2022-04-05 15:44:33 -0500

    swap dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const dict1 = require('./101-data').dict;
+
+let newDict = {};
+
+console.log(dict1);
+
+for (let k in dict1){
+  let keyExists = dict1[k] in newDict;
+
+  if (keyExists) {
+    newDict[dict1[k]].push(k);
+  } else {
+    let arrTemp = [];
+    arrTemp.push(k);
+    newDict[dict1[k]] = arrTemp;
+  }
+}
+
+console.log(newDict);
