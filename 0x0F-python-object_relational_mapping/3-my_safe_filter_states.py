cb238d2efa8286ccbe3b6bba5903b65857918e5b
Carlos Bentin <cbentin12@gmail.com>
2022-04-10 14:10:52 -0500

    fix

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+    task 3
+"""
+
+if __name__ == '__main__':
+
+    import sys
+    import MySQLdb as mysqldb
+
+    host = 'localhost'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    fName = sys.argv[4]
+    port = 3306
+
+    config = {
+        'host': 'localhost',
+        'user': username,
+        'passwd': password,
+        'db': dbname,
+        'port': port
+    }
+
+    db = mysqldb.connect(**config)
+
+    cur = db.cursor()
+
+    query = '''
+        SELECT * FROM states WHERE BINARY
+        name = "{}" ORDER BY id ASC;
+    '''.format(fName)
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
