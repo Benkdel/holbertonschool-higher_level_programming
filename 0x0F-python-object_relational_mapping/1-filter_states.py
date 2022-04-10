fcf90db85c555663923b20e3dff84244d166c291
Carlos Bentin <cbentin12@gmail.com>
2022-04-10 13:56:07 -0500

    first commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+    task 0
+"""
+
+if __name__ == '__main__':
+
+    import sys
+    import MySQLdb as mysqldb
+
+    host = 'localhost'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    port = 3306
+
+    config = {
+        'host': 'localhost',
+        'user': username,
+        'passwd': password,
+        'db': dbname,
+        'port': port
+    }
+
+    db = mysqldb.connect(**config)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
