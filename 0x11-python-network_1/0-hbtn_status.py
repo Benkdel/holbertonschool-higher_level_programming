c29be2dd94d529df84ee9e86b97f59ad482bfd47
Carlos Bentin <cbentin12@gmail.com>
2022-04-22 13:49:02 -0500

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" fetch status code """
+
+import urllib.request
+
+if __name__ == '__main__':
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        res = """Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}
+        """.format(type(body), body, body.decode("utf-8"))
+        print(res)
