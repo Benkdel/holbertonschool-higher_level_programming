d71a50761eb28cf1b5f8cd4e85197525065dff0b
Carlos Bentin <cbentin12@gmail.com>
2022-04-22 13:54:32 -0500

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,10 @@
 
 import urllib.request
 
-if __name__ == '__main__':
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        body = response.read()
-        res = """Body response:
-        - type: {}
-        - content: {}
-        - utf8 content: {}""".format(type(body), body, body.decode("utf-8"))
-        print(res)
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    body = response.read()
+    res = """Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(body), body, body.decode("utf-8"))
+    print(res)
