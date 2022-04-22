a0a085203ddeced478b7b5d53b8aaa962ac3c5f8
Carlos Bentin <cbentin12@gmail.com>
2022-04-22 14:11:27 -0500

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ fetch status code """
 
-import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode("utf-8"))
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Get value from header """
 
-import urllib.request
-import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
