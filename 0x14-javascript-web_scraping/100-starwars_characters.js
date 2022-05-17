40a6feff4aa30c6919a32166145446c6be39ee89
Carlos Bentin <cbentin12@gmail.com>
2022-05-17 16:11:22 -0500

    adv task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,36 +7,53 @@ const movieID = process.argv[2];
 const URL_1 = 'https://swapi-api.hbtn.io/api/films/';
 const URL_2 = 'https://swapi-api.hbtn.io/api/people/';
 
-
-function getMovie() {
-  return axios.get(URL_1 + movieID + '/');
-}
-
-function getCharacters() {
-  return axios.get(URL_2);
-}
-
-Promise.all([getMovie(), getCharacters()])
-  .then(response => {
-    const movie = response[0];
-    const characters = response[1];
-
-    const charUrls = movie.data.characters;
-     
-    console.log(charUrls);
-    console.log(characters.data);
-    /*
-    const charNames = [];
-
-    for (let i = 0; i < charUrls.length; i++) {
-
-    }*/
-
-
-
-
-  }).catch(err => {
-    if (err) {
-      console.log(err.message);
+async function getNextPage (page) {
+  try {
+    const response = await axios.get(`${URL_2}?page=${page}`);
+    if (response.status === 200) {
+      return response.data;
     }
-  });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function getMovie (movieID) {
+  try {
+    const response = await axios.get(`${URL_1}${movieID}/`);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function printAllCharactersofMovie () {
+  const maxPage = 10;
+  let page = 1;
+  let fullCharList = [];
+  let charsInMovie = [];
+
+  /* Get full list of Characters */
+  while (page < maxPage) {
+    const data = await getNextPage(page);
+    page++;
+    fullCharList = fullCharList.concat(data.results);
+    if (data.next === null) {
+      break;
+    }
+  }
+
+  /* get list of characters (URLS) in movie by ID */
+  const data = await getMovie(movieID);
+  charsInMovie = charsInMovie.concat(data.characters);
+
+  for (const c in charsInMovie) {
+    let charID = c.replace('https://swapi-api.hbtn.io/api/people/', '').replace('/', '');
+    charID = parseInt(charID);
+    console.log(`${fullCharList[charID].name}`);
+  }
+}
+
+printAllCharactersofMovie();
