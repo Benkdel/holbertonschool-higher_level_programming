965ba74fa1d27944a20c425950fdd73a47308460
Carlos Bentin <cbentin12@gmail.com>
2022-05-25 10:33:59 -0500

    adding task 7 and 8

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,10 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+
+$(document).ready(()=> {
+
+    url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+    $.getJSON(url, (data) => {
+        $("DIV#character").append(data.name);
+      });
+});
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,17 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+
+$(document).ready(() => {
+
+    
+    const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+    
+    $.getJSON(url, (data) => {
+        const films = [];
+        
+        data.results.forEach(r => {
+            films.push(`<li>${r["title"]}</li>`);
+        });
+
+        $("UL#list_movies").append(films.join(""));
+    });
+});
