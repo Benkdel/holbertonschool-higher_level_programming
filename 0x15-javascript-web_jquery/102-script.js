a5c291cf4f51129ff2890d103492f24f8280a0dd
Carlos Bentin <cbentin12@gmail.com>
2022-05-25 13:21:53 -0500

    adding adv tasks 102 and 103

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,14 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+
+$(document).ready(() => {
+    $('INPUT#btn_translate').click(() => {
+        var lang = $('INPUT#language_code').val();
+        console.log(lang);
+        $('INPUT#language_code').empty();
+
+        var url = `https://www.fourtonfish.com/hellosalut/?lang=${lang}`;
+        $.getJSON(url, (data) => {
+            $('DIV#hello').text(data['hello']);
+        });
+    });
+});
--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,27 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+
+$(document).ready(() => {
+
+    function sayHello() {
+        var lang = $('INPUT#language_code').val();
+        $('INPUT#language_code').empty();
+
+        var url = `https://www.fourtonfish.com/hellosalut/?lang=${lang}`;
+        $.getJSON(url, (data) => {
+            $('DIV#hello').text(data['hello']);
+        });
+    }
+
+    $('INPUT#btn_translate').click(() => {
+        sayHello();
+    });
+
+    $('INPUT#language_code').focus(() => {
+        $(this).keydown((e) => {
+            if (e.which == 13) {
+                sayHello();
+            };
+        });
+    });
+});
+
